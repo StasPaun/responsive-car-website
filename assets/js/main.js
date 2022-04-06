0c788dd4bebe9e552878809e7b07cb72828ade61
Stas Paun <paunstas@gmail.com>
2022-04-06 13:24:08 +0300

    scroll up

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,7 +78,12 @@ function activeFeatured(){
 }
 linkFeatured.forEach(i=> i.addEventListener('click', activeFeatured))
 /*=============== SHOW SCROLL UP ===============*/ 
-
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
